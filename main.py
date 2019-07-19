c9dbfebbd7e75c840d3bda01f389fb73dc1662bb
Joaco <joaquinamf@gmail.com>
2019-07-19 20:59:32 -0300

    Quita segunda señal de la muestra

--- main.py
+++ main.py
@@ -22,19 +22,12 @@ filename = "fetal/NR_04.dat"
 lector = FileHandler()
 tiempo_limite = 30
 ecg, fs, data_original = lector.read_signal_file(filename, tiempo_limite)
-# ecg2, fs2, data_original2 = lector.read_signal_file(filename1, tiempo_limite)
-
 
 ecg.detect_arritmia()
 ecg.print_cardiac_frequency()
 ecg.qualify_cardiac_freq()
 
-#ecg2.detect_arritmia()
-#ecg2.print_cardiac_frequency()
-#ecg2.qualify_cardiac_freq()
-
 pulsos_tiempo, pulsos = ecg.pulsos()
-#pulsos_tiempo2, pulsos2 = ecg2.pulsos()
 
 transformada = ecg.get_transform_values()
 frecuencias = ecg.get_frequency_values()
@@ -44,7 +37,6 @@ _, subplot = plot.subplots(3, 1)
 n = 0
 
 subplot[n].plot(numpy.arange(0, len(data_original)) / fs, data_original)
-#subplot[n].plot(numpy.arange(0, len(data_original2)) / fs2, data_original2)
 subplot[n].set_xlabel('Tiempo')
 subplot[n].set_ylabel('Amplitud')
 subplot[n].title.set_text("Señal Original")
@@ -52,20 +44,16 @@ subplot[n].title.set_text("Señal Original")
 n = n + 1
 
 subplot[n].plot(ecg.time(), ecg.data())
-#subplot[n].plot(ecg2.time(), ecg2.data())
 subplot[n].set_xlabel('Tiempo')
 subplot[n].set_ylabel('Amplitud')
 subplot[n].title.set_text("Señal Recortada")
 
 subplot[n].plot(pulsos_tiempo, pulsos, "ro")
-#subplot[n].plot(pulsos_tiempo2, pulsos2, "go")
 n = n + 1
 
 trans1, freq1 = ecg.transform()
-#trans2, freq2 = ecg2.transform()
 
 subplot[n].plot(freq1, trans1)
-#subplot[n].plot(freq2, trans2)
 subplot[n].set_xlabel('Frecuencia')
 subplot[n].set_ylabel('Modulo')
 subplot[n].title.set_text("Transformada Ventaneada de la Señal Recortada")

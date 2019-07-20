272372e9e17edcb77465a677e72be71da5ff60c6
Joaco <joaquinamf@gmail.com>
2019-07-19 21:13:36 -0300

    Refactoriza comentarios

--- main.py
+++ main.py
@@ -1,18 +1,26 @@
 from tools.FileHandler import FileHandler
 from tools.Visualizador import Visualizador
 
-# filename = "arritmia/100.dat"
-# filename = "ruido/aami3d.dat"
-# filename = "picos_abajo/aami3a.dat"
-# filename = "normal/aami4b_d.dat"
-# filename = "normal/aami4b_h.dat"
-# filename = "fetal/1001.dat"
-# filename1 = "fetal/ARR_01.dat"
-# filename1 = "fetal/NR_01.dat" # OK
-# filename = "fetal/ARR_08.dat" # OK
-# filename = "fetal/ARR_05.dat" # Muchos P
-# filename = "fetal/ARR_03.dat" # Falta P
-# filename = "fetal/NR_03.dat" # Maso
+'''
+filename = "arritmia/100.dat"
+filename = "ruido/aami3d.dat"
+filename = "picos_abajo/aami3a.dat"
+
+Muestras de adulto normales
+
+filename = "normal/aami4b_d.dat"
+filename = "normal/aami4b_h.dat"
+
+Muestras de FECG
+
+filename = "fetal/1001.dat"
+filename1 = "fetal/ARR_01.dat"
+filename1 = "fetal/NR_01.dat" # OK
+filename = "fetal/ARR_08.dat" # OK
+filename = "fetal/ARR_05.dat" # Muchos P
+filename = "fetal/ARR_03.dat" # Falta P
+filename = "fetal/NR_03.dat" # Maso
+'''
 filename = "fetal/NR_04.dat"
 
 lector = FileHandler()

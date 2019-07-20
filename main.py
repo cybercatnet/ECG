8c2921e6689d73ecb5180c59b3bead9b4aff2689
Germán Torós <cybercat_666_78@hotmail.com>
2019-07-19 22:32:09 -0300

    Regulariza archivos

--- main.py
+++ main.py
@@ -4,7 +4,17 @@ from tools.Signal import Signal
 '''
 Muestras de adulto arritmico
 
-filename = "arritmia/100.dat"
+filename = "arritmia/105.dat"
+filename = "arritmia/108.dat"
+filename = "arritmia/119.dat"
+filename = "arritmia/200.dat"
+filename = "arritmia/201.dat"
+filename = "arritmia/203.dat"
+
+Muestras de adulto con bradicardia
+filename = "arritmia/123.dat"
+filename = "arritmia/124.dat"
+filename = "arritmia/202.dat"
 
 Muestras de adulto normales
 
@@ -13,16 +23,13 @@ filename = "normal/aami4b_h.dat"
 
 Muestras de FECG
 
-filename = "fetal/1001.dat"
-filename = "fetal/ARR_01.dat"
 filename = "fetal/NR_01.dat" # OK
 filename = "fetal/ARR_08.dat" # OK
 filename = "fetal/ARR_05.dat" # Muchos P
 filename = "fetal/ARR_03.dat" # Falta P
-filename = "fetal/NR_03.dat" # Maso
 filename = "fetal/NR_04.dat"
 '''
-filename = "arritmia/100.dat"
+filename = "fetal/NR_04.dat"
 
 tiempo_limite = 30
 
